2ac98b067863b4f2d33a107ceae430b682190b24
StelCooin <1411052263@qq.com>
2022-07-26 11:56:17 +0800

    Init

---
+++ src/main/java/com/stelcooin/FirstService.java
@@ -0,0 +1,12 @@
+package com.stelcooin;
+
+/**
+ * @author Liu Yang
+ * @since 2022-07-26
+ */
+public class FirstService {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
